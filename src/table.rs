c2bd525ac67844eb03bcc4e85246e228aa811033
Matan Markind <matan.markind@gmail.com>
2021-05-29 22:42:14 +0300

    Remove stale TODO

--- src/table.rs
+++ src/table.rs
@@ -2,12 +2,6 @@ use crossbeam;
 use std::fmt;
 use std::sync::atomic::{AtomicBool, Ordering};
 
-// TODO: Consider using crossbeam-utils sharded RwLock since it's optimized for fast
-// reads. Since reads should never be contested a faster read implementation
-// seems good. The slower write lock shouldn't be an issue since the slowness on
-// writes that I am worried about is due to reader threads still holding the new
-// 'standby_table' when we try to create a new WriteGuard.
-
 // Define locally the lock types used incase we want to switch to a different
 // implementation.
 pub type RwLock<T> = crossbeam::sync::ShardedLock<T>;
