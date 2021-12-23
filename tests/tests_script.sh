23f2dd803b7ce8de0355b875e6d413c2946574d9
Matan Markind <matan.markind@gmail.com>
2021-12-23 22:33:45 +0200

    Add test_script which is a script with all of the tests that I run. This is both a convenience for running and a record of all the testing methods we use for active_standby.

---
+++ tests/tests_script.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+echo 'cargo clean'
+cargo clean
+
+# Rudra seems to need to be run with sudo & the environment variables didn't
+# seem to stick for me locally. Also running it in sudo creates compilation
+# artifacts that can only be cleaned in sudo, so do that here. Run first so that
+# that user can fill in the sudo password and then leave it to run.
+echo 'sudo RUDRA_RUNNER_HOME=~/rust/Rudra/rudra_runner ~/rust/Rudra/docker-helper/docker-cargo-rudra ~/rust/active_standby && sudo cargo clean'
+sudo RUDRA_RUNNER_HOME=~/rust/Rudra/rudra_runner ~/rust/Rudra/docker-helper/docker-cargo-rudra ~/rust/active_standby && sudo cargo clean
+
+echo 'RUST_BACKTRACE=full cargo test'
+RUST_BACKTRACE=full cargo test
+
+echo 'cargo +nightly bench'
+cargo +nightly bench
+
+echo 'RUSTFLAGS="--cfg loom" cargo +nightly test --test loom --release'
+RUSTFLAGS="--cfg loom" cargo +nightly test --test loom --release
+
+echo 'RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" cargo +nightly test --lib --release -Z build-std --target x86_64-unknown-linux-gnu # tsan'
+RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" cargo +nightly test --lib --release -Z build-std --target x86_64-unknown-linux-gnu # tsan
+
+echo 'RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" cargo +nightly bench -Z build-std --target x86_64-unknown-linux-gnu # tsan'
+RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" cargo +nightly bench -Z build-std --target x86_64-unknown-linux-gnu # tsan
+
+# Miri specifies it should be cleaned beforehand.
+echo 'cargo clean && cargo +nightly miri test && cargo clean'
+cargo clean && cargo +nightly miri test
+
+echo 'cargo clean'
+cargo clean
