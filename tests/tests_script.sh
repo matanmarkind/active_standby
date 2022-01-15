1cda63d197a9e145a851f75b2281401d9aa67e51
Matan Markind <matan.markind@gmail.com>
2022-01-15 20:27:37 +0200

    Update tests_script to repeat less code by creating 'echo_and_run'

--- tests/tests_script.sh
+++ tests/tests_script.sh
@@ -1,80 +1,44 @@
 #! /bin/bash
 
-echo '
+echo_and_run() {
+    echo "
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ sudo cargo clean
+$*
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-sudo cargo clean
+"
+    $*
+}
+
+echo_and_run sudo cargo clean
 
 # Rudra seems to need to be run with sudo & the environment variables didn't
 # seem to stick for me locally. Also running it in sudo creates compilation
 # artifacts that can only be cleaned in sudo, so do that here. Run first so that
 # that user can fill in the sudo password and then leave it to run.
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ sudo RUDRA_RUNNER_HOME=~/rust/Rudra/rudra_runner ~/rust/Rudra/docker-helper/docker-cargo-rudra ~/rust/active_standby
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-sudo RUDRA_RUNNER_HOME=~/rust/Rudra/rudra_runner ~/rust/Rudra/docker-helper/docker-cargo-rudra ~/rust/active_standby
+echo_and_run sudo RUDRA_RUNNER_HOME=~/rust/Rudra/rudra_runner \
+    ~/rust/Rudra/docker-helper/docker-cargo-rudra \
+    ~/rust/active_standby
 
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ sudo cargo clean
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-sudo cargo clean
+echo_and_run sudo cargo clean
 
+echo_and_run RUST_BACKTRACE=full cargo test
 
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ RUST_BACKTRACE=full cargo test
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-RUST_BACKTRACE=full cargo test
+echo_and_run cargo +nightly bench benchmarks
 
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ cargo +nightly bench benchmarks
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-cargo +nightly bench benchmarks
-
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ RUSTFLAGS="--cfg loom" cargo +nightly test --test loom --release
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-RUSTFLAGS="--cfg loom" cargo +nightly test --test loom --release
+echo_and_run RUSTFLAGS="--cfg loom" cargo +nightly test --test loom --release
 
 # tsan requires all libraries to be built with instrumentation, including std,
 # not just the local crate.
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" RUSTDOCFLAGS=-Zsanitizer=thread cargo +nightly bench benchmarks -Z build-std --target x86_64-unknown-linux-gnu # tsan
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" cargo +nightly bench benchmarks -Z build-std --target x86_64-unknown-linux-gnu # tsan
+echo_and_run RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" \
+    cargo +nightly bench benchmarks -Z build-std --target x86_64-unknown-linux-gnu
 
 # tsan doesn't seem to play nice with doc tests, hence the --lib flag.
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" RUSTDOCFLAGS=-Zsanitizer=thread cargo +nightly test --lib --release -Z build-std --target x86_64-unknown-linux-gnu # tsan
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" cargo +nightly test --lib --release -Z build-std --target x86_64-unknown-linux-gnu # tsan
+echo_and_run RUST_BACKTRACE=full RUSTFLAGS="-Zsanitizer=thread -g" \
+    cargo +nightly test --lib --release -Z build-std \
+    --target x86_64-unknown-linux-gnu
 
 # Miri specifies it should be cleaned beforehand.
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ cargo clean && cargo +nightly miri test
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-cargo clean && cargo +nightly miri test
+echo_and_run cargo clean
+echo_and_run cargo +nightly miri test
 
-echo '
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-$ cargo clean
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'
-cargo clean
+echo_and_run cargo clean
