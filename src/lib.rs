a34ddc86ba0e20e392b7a3f032aef7a2c244c7b4
Matan Markind <matan@starkware.co>
2022-05-18 16:46:12 +0300

    Update README/lib docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,88 @@
 //! A library for high concurrency reads.
 //!
-//! This library is named after the 2 (identical) tables that are held
-//! internally:
+//! This library is named after the 2 (identical) tables that are held internally:
 //! - Active - this is the table that all Readers view. This table will never be
 //!   write locked, so readers never face contention.
-//! - Standby - this is the table that the Writer mutates. A writer should face
+//! - Standby - this is the table that the writers mutate. A writer should face
 //!   minimal contention retrieving this table since Readers move to the Active
 //!   table whenever calling `.read()`, so the only contention is long lived
 //!   AsLockReadGuards.
 //!
+//! There are 2 ways to use this crate:
+//! 1. Direct interaction with AsLock. This is highly flexible since users can
+//!    pass in any struct they want. All updates though, will need to be done by
+//!    passing a function, in order to update both tables (`UpdateTables` trait).
+//! 2. Using collections which are built out of the primitives but which provide an
+//!    API similar to RwLock<T>; writers can directly call to methods without
+//!    having to provide a mutator function.
+//!
+//! There are 2 flavors:
+//! 1. Lockless - this variant trades off increased performance against changing the
+//!    API to be less like a `RwLock`. This avoids the cost of performing
+//!    synchronization on reads, but this requires that each thread/task that is
+//!    going to access the tables register in advance. Therefore this centers around
+//!    the `AsLockHandle`, which is conceptually similar to `Arc<RwLock>` (meaning a
+//!    separate `AsLockHandle` per thread/task).
+//! 2. Sync - this centers around using an `AsLock`, which is meant to feel like a
+//!    `RwLock`. These structs can be shared between threads by cloning & sending an
+//!    `Arc<AsLock>` (like with `RwLock`). The main difference is that instead of
+//!    using `AsLock<Vec<T>>`, you would use `vec::sync::AsLock<T>`. This is
+//!    because both tables must be updated, meaning users can't just dereference
+//!    and mutate the underlying table, and so we provide a wrapper class.
+//!
 //! The cost of minimizing contention is:
 //! 1. Memory - Internally there are 2 copies of the underlying type the user
-//!    created. This is needed to allow there to always be a table that Readers
-//!    can access out without contention.
+//!    created. This is needed to allow there to always be a table that Readers can
+//!    access out without contention.
 //! 2. CPU - The writer must apply all updates twice, once to each table. Lock
 //!    contention for the writer should be less than with a plain RwLock due to
-//!    Readers using the active_table, so it's possible that write times
-//!    themselves will drop.
+//!    Readers using the active_table, so it's possible that write times themselves
+//!    will drop.
 //!
-//! The usage is meant to be similar to a RwLock. Some of the inspiration came
-//! from the [left_right](https://crates.io/crates/left-right) crate, so feel
-//! free to check that out. The main differences focus on trying to simplify the
-//! client (creating data structures) and user (using data structures)
-//! experiences. Because there are 2 tables which need to be updated, the user
-//! does not simply grab a writer and mutate the table directly. Rather, users
-//! provider update functions (`UpdateTables` trait) and the crate handles
-//! replaying these updates on both tables. There are 2 ways to interact with
-//! active_standby data structure:
-//! 1. Raw usage of `AsLock<T>`. This provides the `update_tables` interface to
-//!    a user, which takes an update function (or object implementing
-//!    `UpdateTables`) to update both of the tables.
-//! 2. Generating a client which will wrap the `update_tables` interface. This
-//!    provides the user with an interface which imitates a regular
-//!    `RwLockAsLockWriteGuard` (see the `collections` module).
+//! ### Example using `collections`:
+//! ```rust
+//! use std::thread::sleep;
+//! use std::time::Duration;
+//! use std::sync::Arc;
+//! use active_standby::collections::vec as asvec;
 //!
-//! There are 2 flavors of this algorithm that we offer:
-//! 1. Lockless - this variant trades off increased performance against changing
-//!    the API to be less like a `RwLock`. This avoids the cost of performing
-//!    synchronization on reads, but this requires that each thread/task that is
-//!    going to access the tables register in advance. Therefore this centers
-//!    around the `AsLockHandle`, which is conceptually similar to `Arc<RwLock>`
-//!    (meaning a separate `AsLockHandle` per thread/task).
-//! 2. Sync - this centers around using an `AsLock`, which is meant to feel
-//!    like a `RwLock`. These structs can be shared between threads by cloning &
-//!    sending an `Arc<AsLock>` (like with `RwLock`). The main difference is
-//!    that instead of using `AsLock<Vec<T>>`, you would use (e.g.)
-//!    `vec::sync::AsLock<T>`. This is because both tables must be updated,
-//!    meaning users can't just dereference and mutate the underlying table, and
-//!    so we provide a wrapper class.
+//! fn run_lockless() {
+//!     let table = asvec::lockless::AsLockHandle::new(vec![1, 2]);
+//!     let table2 = table.clone();
 //!
-//! An example of where the sync variant can be preferable is a Tonic service.
-//! There you don't spawn a set of tasks/threads where you can pass each of them
-//! a `lockless::AsLockHandle`. You can use a `sync::AsLock` though.
+//!     let handle = std::thread::spawn(move || {
+//!         while *table2.read() != vec![1, 2, 3] {
+//!             sleep(Duration::from_micros(100));
+//!         }
+//!     });
 //!
-//! We provide 2 modules:
-//! 1. primitives - The components used to build data structures in the
-//!    active_standby model. Clients usually don't need to utilize the
-//!    primitives and can instead either utilize the pre-made collections, or
-//!    generate the wrapper for their struct using one of the macros and then
-//!    just implement the mutable API for the generated AsLockWriteGuard.
-//! 2. collections - Sync and lockless active_standby structs for common
-//!    collections. Each table type has its own `AsLock` (sync) /
-//!    `AsLockHandle` (lockless), as opposed to `RwLock` where you simply pass
-//!    in the table. This is because users can't simply gain write access to the
-//!    underlying table and then mutate it. Instead mutations are done through
-//!    UpdateTables so that both tables will be updated.
+//!     table.write().push(3);
+//!     handle.join();
+//! }
 //!
-//! Example creating a wrapper class like in `collections`:
+//! fn run_sync() {
+//!     let table = Arc::new(asvec::sync::AsLock::new(vec![1, 2]));
+//!     let table2 = Arc::clone(&table);
+//!
+//!     let handle = std::thread::spawn(move || {
+//!         while *table2.read() != vec![1, 2, 3] {
+//!             sleep(Duration::from_micros(100));
+//!         }
+//!     });
+//!
+//!     table.write().push(3);
+//!     handle.join();
+//! }
+//!
+//! fn main() {
+//!     run_lockless();
+//!     run_sync();
+//! }
+//! ```
+//!
+//!
+//! ### Example creating a wrapper class like in `collections`:
+//! (For more examples, see the source code in `collections`)
 //! ```rust
 //! use std::thread::sleep;
 //! use std::time::Duration;
@@ -108,32 +124,28 @@
 //! fn run_lockless() {
 //!     let table = lockless::AsLockHandle::new(0);
 //!     let table2 = table.clone();
+//!
 //!     let handle = std::thread::spawn(move || {
 //!         while *table2.read() != 1 {
 //!             sleep(Duration::from_micros(100));
 //!         }
 //!     });
 //!
-//!     {
-//!         let mut wg = table.write();
-//!         wg.add_one();
-//!     }
+//!     table.write().add_one();
 //!     handle.join();
 //! }
 //!
 //! fn run_sync() {
 //!     let table = Arc::new(sync::AsLock::new(0));
 //!     let table2 = Arc::clone(&table);
+//!
 //!     let handle = std::thread::spawn(move || {
 //!         while *table2.read() != 1 {
 //!             sleep(Duration::from_micros(100));
 //!         }
 //!     });
 //!
-//!     {
-//!         let mut wg = table.write();
-//!         wg.add_one();
-//!     }
+//!     table.write().add_one();
 //!     handle.join();
 //! }
 //!
@@ -143,20 +155,18 @@
 //! }
 //! ```
 //!
-//! If your table has large elements, you may want to save memory by only
-//! holding each element once (e.g. `vec::AsLockHandle<Arc<i32>>`). This can be
-//! done safely so long as no elements of the table are mutated, only inserted
-//! and removed. Using a vector as an example, if you wanted a function that
-//! increases the value of the first element by 1, you would not increment the
-//! value behind the Arc. You would reassign the first element to a new Arc with
-//! the incremented value.
+//! If your table has large elements, you may want to save memory by only holding
+//! each element once (e.g. `vec::AsLockHandle<Arc<i32>>`). This can be done
+//! safely so long as no shared objects are mutated. Using a vector as an example,
+//! if you wanted a function that increases the value of the first element by 1,
+//! you would not increment the value behind the Arc. You would reassign the first
+//! element to a new Arc with the incremented value.
 //!
-//! Example of large elements, using the raw `update_tables` interface (see
-//! `UpdateTables` trait):
+//! Example using `primitives` interface:
 //! ```rust
 //! use std::sync::Arc;
 //! use active_standby::primitives::UpdateTables;
-//! use active_standby::primitives::sync::AsLock;
+//! use active_standby::primitives::lockless::AsLockHandle;
 //!
 //! struct UpdateVal {
 //!     index: usize,
@@ -167,20 +177,24 @@
 //!     fn apply_first(&mut self, table: &'a mut Vec<Arc<i32>>) {
 //!         table[self.index] = Arc::clone(&self.val);
 //!     }
+//!
 //!     fn apply_second(mut self, table: &mut Vec<Arc<i32>>) {
 //!         table[self.index] = self.val;
 //!     }
 //! }
 //!
 //! fn main() {
-//!     let table = AsLock::<Vec<Arc<i32>>>::default();
+//!     let table = AsLockHandle::<Vec<Arc<i32>>>::default();
+//!
 //!     table.write().update_tables_closure(
 //!         |table| table.push(Arc::new(1))
 //!     );
+//!
 //!     table.write().update_tables(UpdateVal {
 //!         index: 0,
 //!         val: Arc::new(2)
 //!     });
+//!
 //!     assert_eq!(*table.read(), vec![Arc::new(2)]);
 //! }
 //! ```
@@ -195,8 +209,7 @@
 //!
 //! [loom](https://crates.io/crates/loom)
 //!
-//! [LLVM
-//! Sanitizers](https://doc.rust-lang.org/beta/unstable-book/compiler-flags/sanitizer.html)
+//! [LLVM Sanitizers](https://doc.rust-lang.org/beta/unstable-book/compiler-flags/sanitizer.html)
 //!
 //! [Miri](https://github.com/rust-lang/miri)
 //!
