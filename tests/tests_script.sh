23f15fd7ae68032b813e402f6c8a692c64ee27bf
Matan Markind <matan@starkware.co>
2022-10-23 12:39:06 +0300

    Update tests script.

--- tests/tests_script.sh
+++ tests/tests_script.sh
@@ -13,6 +13,12 @@ $*
 
 echo_and_run cargo clean
 
+echo_and_run cargo +nightly udeps --all-targets
+echo_and_run cargo update
+echo_and_run cargo outdated
+echo_and_run cargo audit
+echo_and_run cargo tree --duplicate
+
 echo_and_run docker-cargo-rudra $PWD
 
 echo_and_run cargo clean
@@ -27,14 +33,13 @@ echo_and_run RUSTFLAGS=\"--cfg loom\" cargo +nightly test --test loom \
 echo_and_run RUSTFLAGS=\"-Zsanitizer=thread -g\" cargo +nightly bench \
     benchmarks -Z build-std --quiet --target x86_64-unknown-linux-gnu
 
-# tsan doesn't seem to play nice with doc tests, hence the --lib flag.
-echo_and_run RUSTFLAGS=\"-Zsanitizer=thread -g\" cargo +nightly test --lib \
-    --release --quiet -Z build-std --target x86_64-unknown-linux-gnu
+echo_and_run RUSTFLAGS=-Zsanitizer=thread RUSTDOCFLAGS=-Zsanitizer=thread \
+    cargo +nightly test -Z build-std --target x86_64-unknown-linux-gnu
 
-echo_and_run RUSTFLAGS=\"-Zsanitizer=address -g\" cargo +nightly test --lib \
-    --release --quiet -Z build-std --target x86_64-unknown-linux-gnu
+echo_and_run RUSTFLAGS=-Zsanitizer=address RUSTDOCFLAGS=-Zsanitizer=address \
+    cargo +nightly test -Z build-std --target x86_64-unknown-linux-gnu
 
-# Miri specifies it should be cleaned beforehand.
+# Miri specifies the crate should be cleaned beforehand.
 echo_and_run cargo clean
 echo_and_run cargo +nightly miri test --quiet
 
